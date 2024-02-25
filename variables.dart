e55b94bfe003d989d0ec62e48e58e6f91c6e7518
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2024-02-25 20:55:47 -0300

    variables

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name = "John Smith";
+  var year = 1997;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'name': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print(name);
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects[3]);
+  print(image.values.first);
+}
